af39b163f235f5df6811c2dbad87cf2fb66902a4
Dai Jiapeng <1557522386@qq.com>
2026-01-20 13:24:48 +0800

    feat(bot_manager): 添加对开发环境tsx loader的支持
    添加tsx作为devDependency以支持开发环境运行.ts文件
    修改bot_manager.ts以在开发环境下为Worker注入tsx loader
    更新package版本至beta-v0.2

--- src/core/bot_manager.ts
+++ src/core/bot_manager.ts
@@ -91,9 +91,15 @@ export class BotManager extends EventEmitter {
 
                 logger.info(`正在启动 Worker: ${symbol}`);
 
+                const execArgv = [...process.execArgv];
+                // 如果是 .ts 文件，说明在开发环境运行 (tsx)，需要为 Worker 注入 tsx loader
+                if (extension === '.ts') {
+                    execArgv.push('--import', 'tsx');
+                }
+
                 const worker = new Worker(workerPath, {
                     workerData: { symbol },
-                    execArgv: process.execArgv
+                    execArgv: execArgv
                 });
 
                 worker.on('message', (msg) => {
